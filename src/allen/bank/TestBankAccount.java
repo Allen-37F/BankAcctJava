d3c355539f45b60e22411e64dc4c53136b8fc038
Allen <afordham@rebelai.com>
2017-05-18 11:25:49 -0600

    Added second test for insufficient funds, although the previous test curiously had 100 percent coverage

--- src/allen/bank/TestBankAccount.java
+++ src/allen/bank/TestBankAccount.java
@@ -17,4 +17,11 @@ public class TestBankAccount {
         Assert.assertEquals(5.0, amount);
     }
 
+    @Test
+    public void testDebitWithInsufficientFunds() {
+        BankAccount account = new BankAccount(10);
+        double amount = account.debit(15);
+        Assert.assertEquals(10.0, amount);
+    }
+
 }
